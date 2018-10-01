871ddd74af15b74c6bea19e4c0586dd2bf6b70d5
JC Albano <jcgalbano@gmail.com>
2018-10-01 17:12:29 +0800

    Fix syntax error on user tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,19 +9,19 @@ RSpec.describe User, type: :model do
   end
 
   it 'is invalid without a user_name' do
-    user_fact = FactoryBot.build(:user, user_name = nil)
+    user_fact = FactoryBot.build(:user, user_name: nil)
 
     expect(user_fact).not_to be_valid
   end
 
   it 'is invalid without an email' do
-    user_fact = FactoryBot.build(:user, email = nil)
+    user_fact = FactoryBot.build(:user, email: nil)
 
     expect(user_fact).not_to be_valid
   end
 
   it 'is invalid without a provided password' do
-    user_fact = FactoryBot.build(:user, password_hash = nil)
+    user_fact = FactoryBot.build(:user, password_hash: nil)
 
     expect(user_fact).not_to be_valid
   end
