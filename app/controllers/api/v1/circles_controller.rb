203402c96c33d6d07a20e5af1097252eaaba24ff
Jan Carlo Albano <jcgalbano@gmail.com>
2018-10-12 19:54:40 +0800

    Add circle creation

--- app/controllers/api/v1/circles_controller.rb
+++ app/controllers/api/v1/circles_controller.rb
@@ -1,6 +1,15 @@
 class Api::V1::CirclesController < ApiController
 
   def create
+  # replace this with http headers
+  request_valid = Circles::Builder.validate(params['user_name'], params['auth_hash'])
+  if request_valid
+    # request_valid returns the user's id if request was valid
+    circle = Circles::Builder.save_circle(params, request_valid)
+    render json: circle
+  else
+    render json: {"error": true}
+  end
   end
 
 end
---
+++ app/services/circles/builder.rb
@@ -0,0 +1,25 @@
+module Circles
+  class Builder
+    def self.save_circle(circle_params, request_valid)
+      circle = Circle.new(circle_name: circle_params[:circle_name],
+                          budget: circle_params[:budget],
+                          exchange_date: circle_params[:exchange_date],
+                          user_id: request_valid)
+      circle.save!
+      circle
+    end
+    def self.validate(user_name, auth_hash)
+      user = User.find_by(user_name: user_name)
+      auth_token = AuthToken.find_by(auth_hash: auth_hash)
+      if auth_token and user
+        if auth_token.user_id == user.id and auth_token.expiry > DateTime.now
+          # returns user id to be used later on
+          # to avoid querying again
+          return user.id
+        end
+      end
+
+      return false
+    end
+  end
+end
