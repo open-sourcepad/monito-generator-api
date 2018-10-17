39618cc0a24cd7b0158d866dbc7a4ef46c0c04df
Jan Carlo Albano <jcgalbano@gmail.com>
2018-10-17 16:52:24 +0800

    Add connection to user and circle

--- app/controllers/api/v1/circles_controller.rb
+++ app/controllers/api/v1/circles_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::CirclesController < ApiController
 
   def index
     user = User.find_by(user_name: params[:user_name])
-    circles = Circle.where(user_id: user.id)
+    circles = user.circles.order('created_at DESC')
     render json: {'circles': circles}
   end
   def create
@@ -11,6 +11,7 @@ class Api::V1::CirclesController < ApiController
     if request_valid
       # request_valid returns the user's id if request was valid
       circle = Circles::Builder.save_circle(params, request_valid)
+      Circles::ConnectionAdder.add_connection(circle['owner'],circle['id'])
       render json: circle
     else
       render json: {"error": true}
@@ -29,12 +30,20 @@ class Api::V1::CirclesController < ApiController
                    'circle_name': circle['circle_name'],
                    'budget': circle['budget'],
                    'exchange_date': circle['exchange_date'],
-                   'owner': circle['owner']
-    }
+                   'owner': circle['owner']}
+    emails_hash = {'existing_emails': []}
 
     users.each do |user|
       email = user['email']
-      UsersMailerWorker.perform_async(circle_hash, email)
+      email_exists = Users::ExistChecker.check_user(email)
+
+      if email_exists
+        emails_hash['existing_emails'].push(email)
+      else
+        UsersMailerWorker.perform_async(circle_hash, email)
+      end
     end
+    render json: emails_hash
+
   end
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -7,6 +7,10 @@ class Api::V1::UsersController < ApiController
     errors = Users::ErrorChecker.check_errors(params)
     if errors.empty?
       user_store = Users::Builder.reg_and_login(params)
+      if params['invited_by']
+        Circles::ConnectionAdder.add_connection(params['user_name'], params['invited_by'])
+      end
+
       render json: user_store
     else
       render json: errors
