cf3771533e81761c04abc4f1885cb38a229382b3
Jan Carlo Albano <jcgalbano@gmail.com>
2018-10-22 17:47:19 +0800

    Include circle_name as response for toastrs

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -14,6 +14,8 @@ class Api::V1::UsersController < ApiController
     if errors.empty?
       user_store = Users::Builder.reg_and_login(params)
       if params['invited_by']
+        circle_name = Circle.find(params['invited_by'])['circle_name']
+        user_store['circle_invitation'] = circle_name;
         Circles::ConnectionAdder.add_connection(params['user_name'], params['invited_by'])
         UserCircles::CodenameBuilder.build_codename(params['user_name'], params['invited_by'], params['code_name'])
       end
@@ -22,8 +24,5 @@ class Api::V1::UsersController < ApiController
     else
       render json: errors
     end
-  end
-
-  def login
   end
 end
