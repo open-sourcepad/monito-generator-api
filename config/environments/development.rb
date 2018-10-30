1bcdd49efca963b75424a8b07d08287a7f62033f
Jan Carlo Albano <jcgalbano@gmail.com>
2018-10-30 21:38:29 +0800

    Setup emailer setting

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -66,12 +66,12 @@ Rails.application.configure do
   # }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_options = {from: 'monitogenerator@gmail.com'}
+  config.action_mailer.default_options = {from: ENV['EMAIL']}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-  address:              'smtp.gmail.com',
+  address:              ENV['ADDRESS'],
   port:                 587,
-  domain:               'gmail.com',
+  domain:               ENV['EMAIL_DOMAIN'],
   user_name:            ENV['EMAIL_USERNAME'],
   password:             ENV['EMAIL_PASSWORD'],
   authentication:       'plain',
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,22 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.delivery_method = :sendmail
+  # Defaults to:
+  # config.action_mailer.sendmail_settings = {
+  #   location: '/usr/sbin/sendmail',
+  #   arguments: '-i'
+  # }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: ENV['EMAIL']}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address:              ENV['ADDRESS'],
+  port:                 587,
+  domain:               ENV['EMAIL_DOMAIN'],
+  user_name:            ENV['EMAIL_USERNAME'],
+  password:             ENV['EMAIL_PASSWORD'],
+  authentication:       'plain',
+  enable_starttls_auto: true  }
 end
